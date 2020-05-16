c8950e3622d47c4cd2558fe7bc254d688b8739a4
Sergey <54347464+coder288@users.noreply.github.com>
2020-05-16 16:49:58 +0300

    Add files via upload

---
+++ precache-manifest.50ee1d4c559ccc361bedb1466265fecf.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61f38b55069a85e414c63c313432bc79",
+    "url": "/weather_app/index.html"
+  },
+  {
+    "revision": "c083efbee0b2197bbed2",
+    "url": "/weather_app/static/css/main.88bec84d.chunk.css"
+  },
+  {
+    "revision": "51c8d257beb0bd9c1608",
+    "url": "/weather_app/static/js/2.06521ddb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weather_app/static/js/2.06521ddb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c083efbee0b2197bbed2",
+    "url": "/weather_app/static/js/main.bcb54178.chunk.js"
+  },
+  {
+    "revision": "fc2d33d06aba44654e8c",
+    "url": "/weather_app/static/js/runtime-main.2ec41269.js"
+  },
+  {
+    "revision": "06924dc32a0cb2c056f0eb10d363220e",
+    "url": "/weather_app/static/media/cloudy.06924dc3.svg"
+  },
+  {
+    "revision": "09dff503a65eb31aef39080611d0a23d",
+    "url": "/weather_app/static/media/moon.09dff503.svg"
+  },
+  {
+    "revision": "576ec571b83a77f646b76fae1175455b",
+    "url": "/weather_app/static/media/night_cloudy.576ec571.svg"
+  },
+  {
+    "revision": "e2602ad44720c57ffb3c7b6db68368bd",
+    "url": "/weather_app/static/media/night_rain.e2602ad4.svg"
+  },
+  {
+    "revision": "b2ddb4585b5c946d02d5749d999e5d25",
+    "url": "/weather_app/static/media/night_snow.b2ddb458.svg"
+  },
+  {
+    "revision": "0bf8659bed21f2cbfa7bfd8dd1fcfa4c",
+    "url": "/weather_app/static/media/rain.0bf8659b.svg"
+  },
+  {
+    "revision": "9668e91c66d0c115ad3c94ef598f62f7",
+    "url": "/weather_app/static/media/snow.9668e91c.svg"
+  },
+  {
+    "revision": "8f2638fa78b8f9324977b8286e03e7a4",
+    "url": "/weather_app/static/media/sun.8f2638fa.svg"
+  },
+  {
+    "revision": "21f64cdd134f676aa9b1d745a7ee95ba",
+    "url": "/weather_app/static/media/sun_cloudy.21f64cdd.svg"
+  },
+  {
+    "revision": "4a0bfa7e3820f958ff5378aa0e40ec0f",
+    "url": "/weather_app/static/media/sun_rain.4a0bfa7e.svg"
+  }
+]);
